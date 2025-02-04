067025854a47f937823c55923d5671d33c6423b0
Karthick Raja <60180945+Karthickraja-S@users.noreply.github.com>
2025-02-04 19:24:21 +0530

    Add files via upload

---
+++ SpringBootAppWithJPA/src/main/java/com/enterprise/app/SpringBootAppWithJPA/SpringBootAppWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.app.SpringBootAppWithJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppWithJpaApplication.class, args);
+	}
+
+}
---
+++ SpringBootAppWithJPA/src/test/java/com/enterprise/app/SpringBootAppWithJPA/SpringBootAppWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.app.SpringBootAppWithJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
